1dbdc1ba361cf6da4b26946b71012ccce1f5f87f
Daniel Jeong <danielyj147@gmail.com>
2026-02-17 13:11:36 -0500

    feat: add lab4 bolierplate

---
+++ lab4/Uppermost.java
@@ -0,0 +1,12 @@
+package lab4;
+
+public class Uppermost {
+    public static int[] visibleLines(double[] slopes, double[] intercepts){
+    }
+    public static void main(String[] args) {
+        double[] slopes = {1, 2, 3, 4};
+        double[] intercepts = {0, 0, 0, 0};
+        int[] result = visibleLines(slopes, intercepts);
+        System.out.println(Arrays.toString(result));
+    }
+}
