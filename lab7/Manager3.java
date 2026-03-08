2038f871c810bc1c457ab2667c1d14d1396cc901
Daniel Jeong <danielyj147@gmail.com>
2026-03-08 18:18:45 -0400

    feat: finish Manager 3

--- lab7/Manager3.java
+++ lab7/Manager3.java
@@ -1,10 +1,17 @@
+import java.util.Comparator;
+import java.util.PriorityQueue;
+import java.util.TreeSet;
+
 public class Manager3 extends DiskManager {
 
-    private Disk current;
+    private DiskComparator c;
+    private PriorityQueue<Disk> q;
 
     public Manager3(int capacity) {
         super(capacity);
-        current = open(); // Open the first disk to start
+        c = new DiskComparator();
+        q = new PriorityQueue<>(c);
+        q.add(open());
     }
 
     @Override
@@ -17,14 +24,28 @@ public class Manager3 extends DiskManager {
             throw new IllegalArgumentException("File size exceeds disk capacity.");
         }
 
-        if (size <= current.getFree()) {
-            current.close();
-            current = open();
+        Disk curr = q.peek();
+
+        
+        if (curr != null && curr.getFree() >= size) {
+            curr = q.poll();
+        } else {
+            curr = open();
         }
 
-        if (!current.assign(fileID, size)) {
-            throw new IllegalStateException(String.format("Failed to assign file (ID=%d, size=%d)\nto disk %s", fileID, size, current.toString()));
+        if (!curr.assign(fileID, size)) {
+            throw new IllegalStateException(String.format("Failed to assign file (ID=%d, size=%d)\nto disk %s", fileID, size, curr.toString()));
         }
+        q.add(curr);
+    }
+    public class DiskComparator implements Comparator<Disk> {
+        @Override
+        public int compare(Disk firstDisk, Disk seconDisk) {
+            int cmp  = Integer.compare(seconDisk.getFree(), firstDisk.getFree());
+            if (cmp != 0) return cmp;
+            return Integer.compare(firstDisk.getID(), seconDisk.getID()); // when equal, return smaller id first
+        }
+
     }
 
     public static void main(String[] args) {
