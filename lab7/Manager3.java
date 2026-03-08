383c890155c9e986f310604307892ae689cda129
Daniel Jeong <danielyj147@gmail.com>
2026-03-08 17:08:50 -0400

    chore: add lab7 2-4 templates

---
+++ lab7/Manager3.java
@@ -0,0 +1,39 @@
+public class Manager3 extends DiskManager {
+
+    private Disk current;
+
+    public Manager3(int capacity) {
+        super(capacity);
+        current = open(); // Open the first disk to start
+    }
+
+    @Override
+    public void assignFile(int fileID, int size) {
+        if (size <= 0) {
+            throw new IllegalArgumentException("File size must be positive.");
+        }
+
+        if (size > getCapacity()) {
+            throw new IllegalArgumentException("File size exceeds disk capacity.");
+        }
+
+        if (size <= current.getFree()) {
+            current.close();
+            current = open();
+        }
+
+        if (!current.assign(fileID, size)) {
+            throw new IllegalStateException(String.format("Failed to assign file (ID=%d, size=%d)\nto disk %s", fileID, size, current.toString()));
+        }
+    }
+
+    public static void main(String[] args) {
+        Manager3 manager = new Manager3(100);
+        manager.assignFile(1, 50);
+        manager.assignFile(2, 30);
+        manager.assignFile(3, 40);
+        manager.assignFile(4, 10);
+        System.out.println(manager);
+    }
+    
+}
